050da0817306af01fdec9b3dcb5821b2f9a9d97a
Danny Barbour <danny.barbour@rubrik.com>
2021-04-06 15:49:44 -0700

    force-yes

--- provision.sh
+++ provision.sh
@@ -54,7 +54,7 @@ rm -f awscliv2.zip
 
 # node & yarn
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs --force-yes
 sudo apt-get remove cmdtest
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
